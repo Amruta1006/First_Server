8372abdcf3e4e48c537a8d36a377471743d39dba
Amruta <amrutabmaskar@gmail.com>
2025-03-28 13:37:40 +0530

    check various postman methods

--- index.js
+++ index.js
@@ -23,6 +23,13 @@ app.get("/about", (req, res) => {
         message: "Contact Page",
     });
    });
+   
+    //API Endpoint 4
+   app.get("/students", (req, res) => {
+    res.json({
+        students: ["Amruta","Shree","Kukii"],
+    });
+   });
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
