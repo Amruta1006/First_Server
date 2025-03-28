ec3eaa28799cf089e792b2b447ade46624701141
Amruta <amrutabmaskar@gmail.com>
2025-03-28 12:55:38 +0530

    create two API endpoints and install nodemon

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 import express from 'express';
 
 const app = express();
-const PORT = 5002;
+const PORT = 5001;
 
-app.get('/ping', (req, res) => {
-    res.send('pong');
+//API Endpoint 1
+app.get("/", (req, res) => {
+    res.send("Hello World");
     });
+
+//API Endpoint 2
+app.get("/about", (req, res) => {
+    res.send("About Page"); 
+   });
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
